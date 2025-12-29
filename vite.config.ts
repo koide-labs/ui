d1e95077e534068711791ab50f26832b70e3dc54
Nathan Pham <npham.dev@gmail.com>
2025-12-29 14:57:32 -0600

    wip: precommit hooks and other packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import compiled from "@compiled/vite-plugin";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [tsconfigPaths(), dts(), compiled(), react()],
+  build: {
+    minify: "esbuild",
+    lib: {
+      entry: {
+        index: path.resolve(__dirname, "src/index.ts"),
+      },
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react/jsx-runtime": "react/jsx-runtime",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    copyPublicDir: false,
+  },
+});
